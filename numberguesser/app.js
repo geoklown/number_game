6ded5238287da4df0b2471a641ace46a4a59565a
John Junkins <jj@j6srvs.com>
2018-05-31 16:49:44 -0500

    DRY

--- numberguesser/app.js
+++ numberguesser/app.js
@@ -6,26 +6,21 @@ GAME FUNCTION:
 -Notify Player if the correct if lose
 -Let Player choose to play again
 */
-
 // Game Values
-
 let min = 1,
   max = 10,
   winningNum = 2,
   guessesLeft = 3;
 // UI Elements
-
 const game = document.querySelector('#game'),
   minNum = document.querySelector('.min-num'),
   maxNum = document.querySelector('.max-num'),
   guessBtn = document.querySelector('#guess-btn'),
   guessInput = document.querySelector('#guess-input'),
   message = document.querySelector('.message');
-
 //Assign UI Min=Max
 minNum.textContent = min;
 maxNum.textContent = max;
-
 //Listen for Guess
 guessBtn.addEventListener('click', function () {
   let guess = parseInt(guessInput.value);
@@ -36,16 +31,31 @@ guessBtn.addEventListener('click', function () {
   }
   //Check if Won
   if (guess === winningNum) {
-    //Disable Input
-    guessInput.disabled = true;
-    guessInput.style.borderColor = 'green';
-    setMessage(`${winningNum} is correct! You Win!`, 'green');
+    gameOver(true, `${winningNum} is correct!, you won`)
   } else {
-
+    //Wrong Number 
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      //Game Over
+      gameOver(false, `Game Over, you lost. The correct answer was ${winningNum}`);
+    } else {
+      //Game Continues Answer Wrong
+      setMessage(`${guess} is incorrect, you have ${guessesLeft} guesses left!`, 'blue');
+      //Change Border Color
+      guessInput.style.borderColor = 'red';
+      // Clear Input
+      guessInput.value = '';
+    }
   }
 });
-
-
+///Game Over Function
+function gameOver(won, msg) {
+  let color;
+  won === true ? color = 'green' : color = 'red'
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMessage(msg);
+}
 ///Set Message
 function setMessage(msg, color) {
   message.style.color = color;
